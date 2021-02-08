6a3135bd5eecf7c53f0e4677eaf7b613e4ff41cf
amanvenkat <78382006+amanvenkat@users.noreply.github.com>
2021-02-09 03:06:28 +0530

    Update memer.js

--- memer.js
+++ memer.js
@@ -116,7 +116,4 @@ memer.on('message', async message => {
     }
 
 });
-var port = process.env.PORT || 3000;
-app.listen(port, "0.0.0.0", function() {
-console.log("Listening on Port 3000");
-});
+
