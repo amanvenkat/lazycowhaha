2df3c26a8af24f50c531d9b3f06d57ff2765d7a6
amanvenkat <78382006+amanvenkat@users.noreply.github.com>
2021-02-09 10:09:49 +0530

    Update memer.js

--- memer.js
+++ memer.js
@@ -9,7 +9,7 @@ mong.connect(process.env.PORT), {
     useFindAndModify: false,
     useNewUrlParser: true,
     useUnifiedTopology: true
-}).then(() => console.log('connected db'));
+}.then(() => console.log('connected db'));
 /** actual bot login things etc */
 const discord = require('discord.js-light');
 const memer = new discord.Client({
